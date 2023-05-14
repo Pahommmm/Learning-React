0e2b2cd460c27e60266d3682e0b3c355a041baa7
Jiajun <wangj180@cardiff.ac.uk>
2023-05-14 18:03:37 +0800

    解决样式丢失

---
+++ 12_解决样式丢失/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {Route,Switch} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Test from './Pages/Test'
+import MyNavLink from './components/MyNavLink'
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <MyNavLink to='/kino/about'>About</MyNavLink>
+              <MyNavLink to='/kino/home'>Home</MyNavLink>
+              <MyNavLink to='/kino/test'>Test</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+              <Switch>
+                <Route path="/kino/about" component={About}/>
+                <Route path="/kino/home" component={Home}/>
+                <Route path="/kino/test" component={Test}/>
+              </Switch>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+/*
+    1.public/index.html中引入样式时 不写./ 写/
+    2.public/index.html中引入样式时 不写./ 写 %PUBLIC_URL%
+    3.使用HashRouter
+
+  */
